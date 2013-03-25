6d0fe87bec1eb73cb91c59ac73d53cacc6491467
nicolas-varaschin <nicovaras22@gmail.com>
2013-03-25 00:02:42 -0300

    url shortener

--- src/url_shortener/core.clj
+++ src/url_shortener/core.clj
@@ -1,6 +1,7 @@
 (ns url-shortener.core
   (:use compojure.handler [compojure.core :only (GET POST defroutes)])
-  (:require [net.cgrand.enlive-html :as en]
+  (:require [compojure.route]
+            [net.cgrand.enlive-html :as en]
             [ring.adapter.jetty :as jetty]
             [ring.util.response :as response])
   (:use clojure.pprint))
@@ -12,22 +13,32 @@
   [url]
   (let [id (swap! counter inc)
         id (Long/toString id 36)]
-    (swap! urls assoc id url)))
+    (swap! urls assoc id url)
+    id))
 
-(en/deftemplate homepage)
+(en/deftemplate homepage
+  (en/xml-resource "homepage.html")
+  [request]
+  [:#listing :li] (en/clone-for [[id url] @urls]
+                                [:a] (en/content (format "%s : %s" id url))
+                                [:a] (en/set-attr :href (str \/ id))))
 
 (defn redirect 
   [id]
   (response/redirect (@urls id)))
 
 
-(defroutes app
+(defroutes app*
+  (compojure.route/resources "/")
   (GET "/" request (homepage request))
+  (POST "/shorten" request (let [id (shorten (-> request :params :url))]
+                             (response/redirect "/")))
   (GET "/:id" [id] (redirect id)))
+
+(def app (compojure.handler/site app*))
 
 (defn run
   []
   (defonce server (jetty/run-jetty #'app {:port 8080 :join? false})))
 
 
-53.02
